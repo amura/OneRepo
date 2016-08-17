483d023d0b03ef910a27275d5c31795b16e5d557
rashid5 <rashid786@gmail.com>
2016-08-17 17:01:01 +0100

    Revert "App_Start migrations"
    This reverts commit 69a833dad6704081f2abc83407b83dfecc75bf0c.

--- OneRepo4/Global.asax.cs
+++ OneRepo4/Global.asax.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -23,10 +22,6 @@ namespace OneRepo4
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            var config = new OneRepo4.Migrations.Configuration();
-            var migrator = new DbMigrator(config);
-            migrator.Update();
         }
     }
 }
