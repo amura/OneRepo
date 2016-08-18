10691adc3e9273b9c8c4ffda6d16186f412c5088
rashid5 <rashid786@gmail.com>
2016-08-18 15:56:09 +0100

    Added custom errors...and some other things

--- OneRepo4/Migrations/Configuration.cs
+++ OneRepo4/Migrations/Configuration.cs
@@ -11,7 +11,7 @@ namespace OneRepo4.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(OneRepo.Controllers.RepoItemContext context)
